944e7117842ced943bff971f9aa3832921515222
Ken Mauch <71083977+KenMauch3@users.noreply.github.com>
2022-01-25 21:30:40 -0500

    Convert name fields to full name complex object

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,41 @@
 import React, { useState } from "react";
 
 function App() {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState({
+    firstName:"",
+    lastName:""
+  });
+  
+  function handleChange(e) {
+    const newValue = e.target.value;
+    const inputName = e.target.name;
 
-  function handleFirstNameChange(e) {
-    setFirstName(e.target.value);
+    if(inputName==="fName"){
+      setFullName({firstName: newValue, lastName: fullName.lastName})
+    } else if (inputName === "lName") {
+      setFullName({firstName: fullName.firstName,
+      lastName: newValue})
+    }
   }
 
-  function handleLastNameChange(e) {
-    setLastName(e.target.value);
-  }
 
   return (
     <div className="container">
       <h1>
-        Hello {firstName} {lastName}
+        Hello {fullName.firstName} {fullName.lastName}
       </h1>
       <form>
         <input
           name="fName"
           placeholder="First Name"
-          onChange={handleFirstNameChange}
-          value={firstName}
+          onChange={handleChange}
+          value={fullName.firstName}
         />
         <input
           name="lName"
           placeholder="Last Name"
-          onChange={handleLastNameChange}
-          value={lastName}
+          onChange={handleChange}
+          value={fullName.lastName}
         />
         <button>Submit</button>
       </form>
